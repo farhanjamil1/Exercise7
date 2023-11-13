1792b5c2b5cbd3b9e53a6e113640776a088bd035
farhanjamil1 <150520713+farhanjamil1@users.noreply.github.com>
2023-11-13 21:21:37 +0500

    Add files via upload
    Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.

---
+++ project7.js
@@ -0,0 +1,8 @@
+var additionResult = 4 + 4;
+console.log("Addition: " + additionResult);
+var subtractionResult = 10 - 2;
+console.log("Subtraction: " + subtractionResult);
+var multiplication = 2 * 4;
+console.log("multiplication: " + multiplication);
+var division = 16 / 2;
+console.log("division: " + division);
---
+++ project7.ts
@@ -0,0 +1,12 @@
+const additionResult= 4 + 4; 
+console.log ("Addition: "+ additionResult)
+
+const subtractionResult= 10-2
+console.log ("Subtraction: "+ subtractionResult)
+
+const multiplication = 2 * 4;
+console.log ("multiplication: "+ multiplication)
+
+const division = 16 / 2;
+console.log ("division: " + division)
+
